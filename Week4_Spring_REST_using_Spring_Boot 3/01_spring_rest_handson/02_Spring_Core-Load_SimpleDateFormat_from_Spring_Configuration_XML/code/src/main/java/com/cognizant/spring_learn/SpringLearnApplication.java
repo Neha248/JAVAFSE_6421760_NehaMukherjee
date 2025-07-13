9c373c2e82000f54767649a165528ea79b63f6a0
Neha Mukherjee <156586318+Neha248@users.noreply.github.com>
2025-07-13 15:38:40 +0530

    weel 4 handson

---
+++ Week4_Spring_REST_using_Spring_Boot 3/01_spring_rest_handson/02_Spring_Core-Load_SimpleDateFormat_from_Spring_Configuration_XML/code/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -0,0 +1,42 @@
+package com.cognizant.spring_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+
+@SpringBootApplication
+public class SpringLearnApplication {
+	
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearnApplication.class, args);
+       displayDate();
+	}
+
+	
+	public static void displayDate() {
+		 ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+		 SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+		 
+		 
+		 try {
+			 Date date =format.parse("13/07/2025");
+			 
+			 System.out.println();
+			 System.out.println(date);
+			 
+		 }
+		 catch (Exception e) {
+			 System.out.println(e.getMessage());
+		 }
+		 
+	}
+}
+	
